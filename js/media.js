6fddc8dadd56c5a57b8871b30be234fd8c50b17a
Joel Seidel <jds6294@psu.edu>
2019-03-16 22:42:53 -0400

    fixed infinite media response generation loop

--- js/media.js
+++ js/media.js
@@ -10,7 +10,7 @@ function handleCoachFiring(moodRating, mediaType, abbr, coach){
     var badMoodResponses = ["REALLY???!! Total shocker, can't believe they would even think about firing " + coach.name + " #" + abbr, "The front office made its decision today to release " + coach.name + ". I guess no one's job is safe...." + " #" + abbr, "#" + abbr + " made the shocking announcement today to fire " + coach.name + ". Will be interesting to see how this goes over in the locker room", coach.name + " was a true great for #" + abbr + ". Firing him will come back to haunt them.", "A moment of silence is being held in " + getTeam(abbr).city + " tonight after their chances this season died with the firing of " + coach.name, coach.name + " has been fired by the " + getTeamFullName(abbr) + " #" + abbr];
     var currentResponses = [];
     if(mediaType === "tweets"){
-        for(var i = 0; i < nflInsiders.length; i += Math.floor(Math.random() * 4)){
+        for(var i = 0; i < nflInsiders.length; i += Math.floor(Math.random() * 5)){
             var thisResponseIndex;
             var thisResponse;
             do{
